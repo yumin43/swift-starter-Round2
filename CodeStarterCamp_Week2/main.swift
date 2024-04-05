37e31b234f83b295d2d9f6f67972d4e927481b75
dsungc1111 <eotjd9808@naver.com>
2024-04-06 01:17:20 +0900

    Step2 피드백반영

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -6,33 +6,42 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+// 전역변수, 지역변수
+// 함수 사용 > 랜덤 로또 번호 저장
+// 네이밍
+// 함수 매개변수 네이밍 모호
+// set특징을 활용해서 컴, 나 의 수에서 동일한 숫자 찾기
+// map에 대해 설명
+
+
+
 import Foundation
 
 let myLottoNumbers: [Int] = [1,2,3,4,5,6]
-var goodLucks = Set<Int>()
-var selectedNumbers: [Int] = []
 
-while (goodLucks.count < 6) {
-    let random = Int.random(in: 1...45)
-    goodLucks.insert(random)
+func pickLottoNum() -> Set<Int> {
+    var lottoNum = Set<Int>()
+    while (lottoNum.count < 6) {
+        let random = Int.random(in: 1...45)
+        lottoNum.insert(random)
+    }
+    return lottoNum
 }
 
-func findSameNumbers(my: [Int], com: Set<Int>){
-    for i in my {
-        for j in com {
-            if i == j {
-                selectedNumbers.append(i)
-            }
-        }
-    }
-    if selectedNumbers.count == 0{
+func findSameNumbers(myNum: [Int]) {
+    
+    let boxOfLottoNum = pickLottoNum()
+    let selectedNumbers: Set<Int> = boxOfLottoNum.intersection(myNum)
+    
+    if selectedNumbers.count == 0 {
         print("아쉽지만 겹치는 번호가 없습니다.")
     } else {
         print("축하합니다! 겹치는 번호는 \(selectedNumbers.map{String($0)}.joined(separator: ",")) 입니다!")
     }
+    
 }
 
 
 
-findSameNumbers(my: myLottoNumbers, com: goodLucks)
+findSameNumbers(myNum: myLottoNumbers)
 
