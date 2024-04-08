d32888c245a900aba0d0c1268ef4ad971a0d4cf2
dsungc1111 <eotjd9808@naver.com>
2024-04-09 01:16:25 +0900

    step3 reupload

--- step3/step3/main.swift
+++ step3/step3/main.swift
@@ -1,11 +1,30 @@
-//
-//  main.swift
-//  step3
-//
-//  Created by 최대성 on 4/9/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+
+func searchLotto(time: String) {
+    var lottoWinningNumber: Dictionary<String, Set> = [String: Set<Int>]()
+    var number: Int = 0
+    var randomPick: Set<Int> = Set<Int>()
+
+    while number < 5 {
+        let round: String = "\(number+1)회차"
+        number = number + 1
+        
+        while randomPick.count < 6 {
+            let computersPick = Int.random(in: 1...45)
+            randomPick.insert(computersPick)
+        }
+        
+        lottoWinningNumber[round] = randomPick
+        randomPick.removeAll()
+    }
+    
+    if let realLotto = lottoWinningNumber[time] {
+        print("\(time)의 로또 당첨 번호는 \(realLotto.map{String($0)}.joined(separator: ", ")) 입니다.")
+    }
+}
+
+
+searchLotto(time: "5회차")
 
