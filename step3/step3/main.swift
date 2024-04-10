fd35720f1ce6aac58528b92a55b9318850fc4de9
dsungc1111 <eotjd9808@naver.com>
2024-04-10 12:47:50 +0900

    step3 피드백반영

--- step3/step3/main.swift
+++ step3/step3/main.swift
@@ -2,29 +2,32 @@
 import Foundation
 
 
-func searchLotto(time: String) {
-    var lottoWinningNumber: Dictionary<String, Set> = [String: Set<Int>]()
-    var number: Int = 0
-    var randomPick: Set<Int> = Set<Int>()
+var lottoWinningNumber: Dictionary<String, Set> = [String: Set<Int>]()
+var number: Int = 0
 
-    while number < 5 {
-        let round: String = "\(number+1)회차"
-        number = number + 1
-        
-        while randomPick.count < 6 {
-            let computersPick = Int.random(in: 1...45)
-            randomPick.insert(computersPick)
-        }
-        
-        lottoWinningNumber[round] = randomPick
-        randomPick.removeAll()
+func findLottoNumber() -> Set<Int> {
+    var randomPick: Set<Int> = Set<Int>()
+    while randomPick.count < 6 {
+        let computersPick = Int.random(in: 1...45)
+        randomPick.insert(computersPick)
     }
+    return randomPick
+}
+
+while number < 5 {
+    let round: String = "\(number+1)회차"
+    lottoWinningNumber[round] = findLottoNumber()
+    number = number + 1
     
+}
+
+func searchLotto(time: String) {
     if let realLotto = lottoWinningNumber[time] {
         print("\(time)의 로또 당첨 번호는 \(realLotto.map{String($0)}.joined(separator: ", ")) 입니다.")
     }
 }
 
-
 searchLotto(time: "5회차")
+
+
 
