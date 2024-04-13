1299908a868b952b60c4553b5f79b23af32ec5b3
ssjooon <sjoon97@naver.com>
2024-04-13 16:51:21 +0900

    [Round2] Step3 - 함수명 변경

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,14 +1,14 @@
 var roundNumbers: Dictionary<String, Set<Int>> = [String: Set<Int>]()
 
-func searchWinningNumber (round: Int) {
+func searchWinningNumber(round: Int) {
     if let winningNumber = roundNumbers["\(round)회차"] {
-        print("\(round)회차의 로또 당첨 번호는 \(numbersToStr(numbers: winningNumber)) 입니다.")
+        print("\(round)회차의 로또 당첨 번호는 \(makeList(numbers: winningNumber)) 입니다.")
     } else {
         print("\(round)회차는 아직 추첨되지 않았습니다.")
     }
 }
 
-func numbersToStr (numbers: Set<Int>) -> String{
+func makeList(numbers: Set<Int>) -> String {
     return numbers.map{String($0)}.joined(separator: ", ")
 }
 
