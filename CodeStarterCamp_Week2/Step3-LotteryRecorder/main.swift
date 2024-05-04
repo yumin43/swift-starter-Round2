a86c0bb3feec0b3a19ab328da1a986f0e02eef24
peat <ldc1000@naver.com>
2024-05-04 19:21:24 +0900

    Step 3: 로또 당첨 번호를 회차별로 저장하고, 확인하자!

---
+++ CodeStarterCamp_Week2/Step3-LotteryRecorder/main.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  Step3-save-Lotto
+//
+//  Created by 이재영 on 2024/05/04.
+//
+
+import Foundation
+
+func convertToString(numbers: Array<Int>, separator: String = ", ") -> String {
+    return numbers.map { "\($0)" }.joined(separator: separator)
+}
+
+func lotteryMachine() -> [Int] {
+    var numbers: Set<Int> = Set(1...45)
+    var winningNumber: Set<Int> = []
+    
+    while winningNumber.count < 6 {
+        winningNumber.insert(numbers.first!)
+        numbers.removeFirst()
+    }
+    return winningNumber.sorted()
+}
+
+func lotteryRecorder(rounds : Int) -> [String : [Int]] {
+    guard rounds > 0 else {
+        return [:]
+    }
+    
+    var lottoNumberOfRound : [String : [Int]] = [:]
+    
+    for round in 1...rounds {
+        lottoNumberOfRound["\(round)회차"] = lotteryMachine()
+    }
+    return lottoNumberOfRound
+}
+
+func lottertyRecordMessenger(round: Int, lottoNumberRecords : [String : [Int] ] ) {
+    guard let lotteryNumber = lottoNumberRecords["\(round)회차"] else {
+        print("기록되지 않은 회차입니다.")
+        return
+    }
+
+    print("\(round)회차의 로또 당첨 번호는 " + convertToString(numbers: lotteryNumber) + " 입니다.")
+}
+
+let lotteryRecords = lotteryRecorder(rounds: 5)
+
+lottertyRecordMessenger(round : 1,  lottoNumberRecords: lotteryRecords)
+lottertyRecordMessenger(round : 2,  lottoNumberRecords: lotteryRecords)
+lottertyRecordMessenger(round : 3,  lottoNumberRecords: lotteryRecords)
+lottertyRecordMessenger(round : 4,  lottoNumberRecords: lotteryRecords)
+lottertyRecordMessenger(round : 5,  lottoNumberRecords: lotteryRecords)
