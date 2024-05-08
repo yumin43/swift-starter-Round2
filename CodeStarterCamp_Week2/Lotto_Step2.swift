1913e3244e45ed494fc70e86aea230e7364fa808
SeBin-Kwon <sbkwon98@gmail.com>
2024-05-08 23:42:42 +0900

    Add makeLottoNumbers func

--- CodeStarterCamp_Week2/Lotto_Step2.swift
+++ CodeStarterCamp_Week2/Lotto_Step2.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+
+func makeLottoNumbers() -> Set<Int> {
+    var winningLottoSet = Set<Int>()
+    while winningLottoSet.count < 6 {
+        winningLottoSet.insert(Int.random(in: 1...45))
+    }
+    return winningLottoSet
+}
+
+
