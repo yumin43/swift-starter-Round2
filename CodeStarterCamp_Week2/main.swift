c6b3cf31bf0c315ff562b555e310a1a8871dbcdf
김다빈 <mokdb17@gmail.com>
2024-05-16 00:39:31 +0900

    [Feat] add round2

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
 
+func selectLottoNumbers() -> Set<Int> {
+    var lottoNums: Set<Int> = []
+    
+    while lottoNums.count < 6 {
+        let randomNum = Int.random(in: 1...45)
+        lottoNums.insert(randomNum)
+    }
+    
+    return lottoNums
+}
+
+func checkNumber(with myNums: Set<Int>, to lottoNums: Set<Int>) {
+    if myNums.intersection(lottoNums).count == 0 {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    } else {
+        let commonNumbers = myNums.intersection(lottoNums).map { String($0) }.joined(separator: ", ")
+        print("축하합니다! 겹치는 번호는 \(commonNumbers) 입니다!")
+    }
+}
+
+let myNumbers = selectLottoNumbers()
+let lottoNumbers = selectLottoNumbers()
+
+checkNumber(with: myNumbers, to: lottoNumbers)
