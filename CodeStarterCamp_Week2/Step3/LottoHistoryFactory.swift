d5ce3d6ebd218636fd7702f3f5290f439b4d1e30
kingkiboots <tuffhr4@gmail.com>
2024-06-22 22:01:14 +0900

    style: 불필요한 주석 제거

--- CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
+++ CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
@@ -12,17 +12,15 @@ let combineIndexWithLottoHistorySuffix = { (index: Int) -> String in
     return "\(index)\(lottoHistorySuffix)"
 }
 
-// 스위프트 컴파일러의 타입추론에 대해서 궁금했다. 타입추론의 범위가 과연 어디까지 일까. 어떤 거는 타입추론을 해야하는 것이고, 어떤 것은 타입 추론이라는 과정이 들어가지 않아서 속도에 영향을 전혀 안줄까?
 func generateLottoHistories() -> Dictionary<String, [Int]> {
     var lottoHistories: Dictionary<String, [Int]> = [:]
     
-    // 새로운 로또 당첨 번호를 생성하면, 직전 회차에 1을 더해 금번 회차를 만들어냅니다.
-    var index: Int = 1
-    while index < 6 {
+    var index: Int = 0
+    while index < 5 {
+        index += 1
         let lottoNumbers: Set<Int> = generateLottoNumbers()
         let lottoHistoryIndex: String = combineIndexWithLottoHistorySuffix(index)
         lottoHistories[lottoHistoryIndex] = Array(lottoNumbers).sorted()
-        index += 1
     }
     return lottoHistories
 }
