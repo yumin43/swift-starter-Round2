b8ea6bb31cf09f7688bee0ad0631b3343b51f2f4
kingkiboots <tuffhr4@gmail.com>
2024-06-22 20:21:09 +0900

    로또 당첨 히스토리 key값을 suffix와 붙여주는 함수 추가

--- CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
+++ CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+
+let combineIndexWithLottoHistorySuffix = { (index: Int) -> String in
+    let lottoHistorySuffix = "회차"
+    return "\(index)\(lottoHistorySuffix)"
+}
+
 // 스위프트 컴파일러의 타입추론에 대해서 궁금했다. 타입추론의 범위가 과연 어디까지 일까. 어떤 거는 타입추론을 해야하는 것이고, 어떤 것은 타입 추론이라는 과정이 들어가지 않아서 속도에 영향을 전혀 안줄까?
 func generateLottoHistories() -> Dictionary<String, [Int]> {
     var lottoHistories: Dictionary<String, [Int]> = [:]
@@ -15,7 +21,7 @@ func generateLottoHistories() -> Dictionary<String, [Int]> {
     var index: Int = 1
     while index < 6 {
         let lottoNumbers: Set<Int> = generateLottoNumbers()
-        let lottoHistoryIndex: String = "\(index)회차"
+        let lottoHistoryIndex: String = combineIndexWithLottoHistorySuffix(index)
         lottoHistories[lottoHistoryIndex] = Array(lottoNumbers).sorted()
         index += 1
     }
@@ -24,7 +30,7 @@ func generateLottoHistories() -> Dictionary<String, [Int]> {
 
 func findLottoHistory(at index: Int) {
     let lottoHistories: Dictionary<String, [Int]> = generateLottoHistories()
-    let lottoHistoryIndex: String = "\(index)회차"
+    let lottoHistoryIndex: String = combineIndexWithLottoHistorySuffix(index)
     
     guard let selectedLottoHistory = lottoHistories[lottoHistoryIndex] else {
         print("\(lottoHistoryIndex)에는 로또 당첨 번호가 없어요")
