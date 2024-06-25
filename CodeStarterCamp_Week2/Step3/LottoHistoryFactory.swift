a4009b4f2e8891032846221df1fbe09386518290
kingkiboots <tuffhr4@gmail.com>
2024-06-25 23:21:16 +0900

    refactir: 색인을 뜻하는 index보다는 회차를 뜻하는 round로 변수 및 상수명 변경

--- CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
+++ CodeStarterCamp_Week2/Step3/LottoHistoryFactory.swift
@@ -10,23 +10,23 @@ import Foundation
 func generateLottoHistories() -> Dictionary<Int, [Int]> {
     var lottoHistories: Dictionary<Int, [Int]> = [:]
     
-    var index: Int = 0
-    while index < 5 {
-        index += 1
+    var round: Int = 0
+    while round < 5 {
+        round += 1
         let lottoNumbers: Set<Int> = generateLottoNumbers()
-        lottoHistories[index] = Array(lottoNumbers).sorted()
+        lottoHistories[round] = Array(lottoNumbers).sorted()
     }
     return lottoHistories
 }
 
-func findLottoHistory(index: Int) {
+func findLottoHistory(round: Int) {
     let lottoHistories: Dictionary<Int, [Int]> = generateLottoHistories()
     
-    guard let selectedLottoHistory = lottoHistories[index] else {
-        print("\(index)회차에는 로또 당첨 번호가 없어요")
+    guard let selectedLottoHistory = lottoHistories[round] else {
+        print("\(round)회차에는 로또 당첨 번호가 없어요")
         return
     }
     
     let lottoHistoryResult: String = selectedLottoHistory.map(String.init).joined(separator: ", ")
-    print("\(index)회차의 로또 당첨 번호는 \(lottoHistoryResult) 입니다.")
+    print("\(round)회차의 로또 당첨 번호는 \(lottoHistoryResult) 입니다.")
 }
