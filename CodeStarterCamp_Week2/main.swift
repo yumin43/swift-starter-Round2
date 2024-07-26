b0eaf8b5f3552e85b7aac075319169d631ff1b87
yumin43 <amily8050@gmail.com>
2024-07-26 18:25:22 +0900

    Step1 v2 commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,1 +1,12 @@
+import Foundation
 
+var winLottoNumbers: Set<Int> = Set<Int>()
+
+while winLottoNumbers.count < 6 {
+    var number = Int.random(in: 1...45)
+    winLottoNumbers.insert(number)
+}
+
+print(winLottoNumbers)
+
+let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
