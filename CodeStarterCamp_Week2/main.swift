1b2d05e89c6bb8f97d8a9bfdfb1292a1f53c52ef
yumin43 <amily8050@gmail.com>
2024-08-07 16:13:16 +0900

    Step3 v1 commit

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -8,24 +8,36 @@ func createWinLottoNumbers() -> Set<Int> {
         winLottoNumbers.insert(number)
     }
     
+    saveDict(winLottoNumbers: winLottoNumbers)
     return winLottoNumbers
 }
 
 let myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
+var winLottoNumbersDict: [String: [Int]] = [:]
+var roundNum: Int = 1
+
+func saveDict(winLottoNumbers: Set<Int>) {
+    var roundNumString: String = "\(roundNum)회차"
+    var winLottoNumbersArray: [Int] = Array(winLottoNumbers)
+    winLottoNumbersDict[roundNumString] = winLottoNumbersArray
+    roundNum += 1
+}
 
 func checkSameNumbers(winLottoNumbers: Set<Int>) {
     let sameNumbers: Set<Int> = winLottoNumbers.intersection(myLottoNumbers)
-    // Int 형태 set을 문자열로 전환
     let sameNumbersString = sameNumbers.map { String($0) }
-    // 문자열 Set을 ,로 구분하여 하나의 문자열로 결합
     let sameNumbersJoin: String = sameNumbersString.joined(separator: ", ")
-    
-    if sameNumbers.isEmpty {
-        print("아쉽지만 겹치는 번호가 없습니다.")
-    } else {
-        print("축하합니다! 겹치는 번호는 \(sameNumbersJoin) 입니다!")
-    }
 }
 
-checkSameNumbers(winLottoNumbers: createWinLottoNumbers())
+for _ in 1...5 {
+    checkSameNumbers(winLottoNumbers: createWinLottoNumbers())
+}
 
+// print(winLottoNumbersDict)
+
+let myRoundNum: String = "2회차"
+if let myRoundWinLottoNumbers = winLottoNumbersDict[myRoundNum] {
+    let myRoundWinLottoNumbersString = myRoundWinLottoNumbers.map { String($0) }
+    let myRoundWinLottoNumbersJoin: String = myRoundWinLottoNumbersString.joined(separator: ", ")
+    print("\(myRoundNum)의 로또 당첨 번호는 \(myRoundWinLottoNumbersJoin) 입니다.")
+}
